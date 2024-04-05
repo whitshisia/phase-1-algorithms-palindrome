9e7ae9ee1cdcc69062b5c09ce1313378894c33b3
whitney shisia <shisiawhitney215@gmail.com>
2024-04-05 07:15:23 +0300

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const cleanStr = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+        return cleanStr === cleanStr.split('').reverse().join('');
 }
+console.log(isPalindrome("robot"))
 
 /* 
   Add your pseudocode here
+  input = "aWord"
+  output ="true| false"
 */
 
 /*
   Add written explanation of your solution here
+  the function takes in a word and if the word is a palindrome it returns a true else it
+  returns false 
+
 */
 
 // You can run `node index.js` to view these console logs
